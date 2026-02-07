a7b0a62ae6f5b4fd07293917d0447d0f2a2b17b8
eveningb4dawn <aryanduntley@gmail.com>
2026-02-06 20:01:52 -0400

    .gitignore

--- src/aifp/wrappers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-AIFP Wrappers - FP-Compliant Wrappers for External Libraries
-
-All external OOP-based libraries are wrapped here to maintain
-FP compliance throughout the codebase. Each wrapper module isolates
-side effects and exposes pure functional interfaces where possible.
-"""
